3afd7987dcf5a6ec28c0e358afb2b8689bbca9d9
Lucas Jandrey <wdev.lucas@gmail.com>
2020-10-26 22:58:14 -0300

    Adding jwt secret to settings example file

--- src/settings.example.ts
+++ src/settings.example.ts
@@ -4,8 +4,9 @@ import path from "path";
  * Remove example from filename and fill constants
  */
 export const DEV_MODE = 1;
-export const MONGO_DNS = 'mongodb://localhost:27017/bipark';
+export const MONGO_DNS = '';
 export const SENDINBLUE_KEY = '';
+export const JWT_SECRET = '';
 
 export const GLOBAL_PATHS = {
   LOGS: path.join(__dirname, '/logs'),
